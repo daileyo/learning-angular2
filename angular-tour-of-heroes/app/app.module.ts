8f19d5e3a109a0b2bfec0c21aa3e2236580de882
Robert Dailey <Robert Dailey>
2016-11-15 19:33:10 -0800

    Register for HTTP services

--- angular-tour-of-heroes/app/app.module.ts
+++ angular-tour-of-heroes/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent }   from './app.component';
 import { HeroDetailComponent} from  './hero-detail.component';
@@ -13,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
   imports:  [ 
       BrowserModule,
       FormsModule,
+      HttpModule,
       AppRoutingModule
   ],
   declarations: [ 
@@ -21,9 +23,7 @@ import { AppRoutingModule } from './app-routing.module';
       HeroDetailComponent,
       HeroesComponent
       ],
-  providers: [
-      HeroService
-  ],
+  providers: [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
 
